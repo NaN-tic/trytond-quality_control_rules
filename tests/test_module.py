b7535680e362aa5475f072d426ca2eaf93897814
Àngel Àlvarez <angel@nan-tic.com>
2026-05-26 17:49:56 +0200

    addd misssing files

---
+++ tests/test_module.py
@@ -0,0 +1,12 @@
+# This file is part quality_control_rules module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from trytond.tests.test_tryton import ModuleTestCase
+
+
+class QualityControlRulesTestCase(ModuleTestCase):
+    'Test Quality Control Rules module'
+    module = 'quality_control_rules'
+
+
+del ModuleTestCase
